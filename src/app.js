fac471358f75db409fdcf411aa01182812803519
SantiFP <santiagoferking@gmail.com>
2023-06-04 21:14:28 -0300

    Listo

--- src/app.js
+++ src/app.js
@@ -3,15 +3,109 @@ const express = require("express");
 const app = express();
 const routerProducts = require("../routes/routerProducts");
 const routerCart = require("../routes/routerCart");
+const exphbs = require("express-handlebars").create({});
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
+//Express
 app.use(express.json());
+const PORT = 8080;
 
-const PORT = 3000;
+//Socket
+// const io = socketIO(server);
+
+//Handlebars
+app.engine("handlebars", exphbs.engine);
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "handlebars");
+
+const ProductManager = require("../utilities/productManager");
+const productManager = new ProductManager(
+  path.join(__dirname, "../data/productos.json")
+);
+
+app.get("/", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    res.render("home", { products });
+  } catch (error) {
+    console.error("Error retrieving products:", error);
+    res.status(500).send("Internal server error");
+  }
+});
 
 app.use("/api/products", routerProducts);
 
 app.use("/api/carts", routerCart);
 
-app.listen(PORT, () => {
+app.use(
+  "/socket.io",
+  express.static(path.join(__dirname, "node_modules/socket.io/client-dist"))
+);
+
+// app.listen(PORT, () => {
+//   console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+// });
+
+//Real Time
+// Ruta para renderizar la vista realTimeProducts.handlebars
+app.get("/realtimeproducts", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    res.render("realTimeProducts", { products });
+  } catch (error) {
+    console.error("Error retrieving products:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+// ...
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  // Escuchar eventos de cambio en la lista de productos
+  productManager.on("change", (products) => {
+    // Emitir evento 'update' con la lista actualizada de productos
+    socket.emit("update", products);
+  });
+
+  // Manejar evento 'createProduct' para crear un nuevo producto
+  socket.on("createProduct", async (product) => {
+    try {
+      await productManager.addProduct(product);
+
+      // Emitir evento 'update' con la lista actualizada de productos a todos los clientes
+      io.emit("update", await productManager.getProducts());
+
+      console.log(`Product created with ID: ${product}`);
+    } catch (error) {
+      console.error("Error creating product:", error);
+    }
+  });
+
+  // Manejar evento 'deleteProduct' para eliminar un producto
+  socket.on("deleteProduct", async (productId) => {
+    try {
+      await productManager.deleteProductById(productId);
+
+      // Emitir evento 'update' con la lista actualizada de productos a todos los clientes
+      io.emit("update", await productManager.getProducts());
+
+      console.log(`Product deleted with ID: ${productId.id}`);
+    } catch (error) {
+      console.error("Error deleting product:", error);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// ...
+
+server.listen(PORT, () => {
   console.log(`Servidor Express escuchando en el puerto ${PORT}`);
 });
--- utilities/productManager.js
+++ utilities/productManager.js
@@ -1,11 +1,12 @@
-const fs = require('fs').promises;
+const fs = require("fs").promises;
+const EventEmitter = require("events");
 
-
-class ProductManager {
+class ProductManager extends EventEmitter {
   constructor(path) {
+    super();
     this.path = path;
     this.products = [];
-    this.loadProductsFromFile();
+    this.loadProductsFromFile(); // Carga inicial de productos desde el archivo
   }
 
   async saveProductsToFile(products) {
@@ -15,117 +16,86 @@ class ProductManager {
 
   async loadProductsFromFile() {
     try {
-      const data = await fs.readFile(this.path, 'utf-8');
-      this.products = JSON.parse(data);
+      const data = await fs.readFile(this.path, "utf-8");
+      const parsedProducts = JSON.parse(data);
+      this.products = parsedProducts;
     } catch (error) {
-      console.error('Error loading products from file:', error);
+      console.error("Error loading products from file:", error);
     }
   }
 
   async getProducts() {
-    await this.loadProductsFromFile();
-    return this.products;
+    return this.products; // Devuelve la lista actual de productos en memoria
   }
 
   async getProductById(id) {
-    const productId = parseInt(id); // Realizar parsing del id como entero
+    const productId = parseInt(id);
     const product = this.products.find((p) => p.id === productId);
     if (!product) {
-      throw new Error('Product not found');
+      throw new Error("Product not found");
     }
     return product;
   }
-  
 
   async getProductByCode(code) {
     const product = this.products.find((p) => p.code === code);
     if (!product) {
-      throw new Error('Product not found');
+      throw new Error("Product not found");
     }
     return product;
   }
-  
-  
 
   async addProduct(product) {
-    await this.loadProductsFromFile();
-    const existingProductIndex = this.products.findIndex((p) => p.code === product.code);
-  
-    if (existingProductIndex !== -1) {
-      // Actualizar el producto existente
-      const existingProduct = this.products[existingProductIndex];
-      existingProduct.title = product.title;
-      existingProduct.description = product.description;
-      existingProduct.price = product.price;
-      existingProduct.stock = product.stock;
-      existingProduct.category = product.category;
-      existingProduct.thumbnails = product.thumbnails;
-      await this.saveProductsToFile(this.products);
-      return existingProduct.id;
-    }
-  
-    // Agregar un nuevo producto
     const id = this.generateProductId();
     product.id = id;
     this.products.push(product);
-    await this.saveProductsToFile(this.products);
+    await this.saveProductsToFile(this.products); // Guarda los productos en el archivo
     return id;
   }
 
-async updateProduct(id, updatedFields) {
-  await this.loadProductsFromFile();
-  const product = this.products.find((p) => p.id === id);
-  if (!product) {
-    throw new Error('Product not found');
-  }
-  Object.assign(product, updatedFields);
-  await this.saveProductsToFile(this.products);
-}
-
-async updateProductByCode(code, updatedFields) {
-  await this.loadProductsFromFile();
-  const productIndex = this.products.findIndex((p) => p.code === code);
-  if (productIndex === -1) {
-    throw new Error('Product not found');
-  }
-  const product = this.products[productIndex];
-  Object.assign(product, updatedFields);
-  await this.saveProductsToFile(this.products);
-}
-
-
-async deleteProductById(id) {
-  const productId = parseInt(id); // Parsear el id como entero
-  await this.loadProductsFromFile();
-  const index = this.products.findIndex((p) => p.id === productId);
-  if (index === -1) {
-    throw new Error('Product not found');
-  }
-  const deletedProduct = this.products.splice(index, 1);
-  await this.saveProductsToFile(this.products);
-  return deletedProduct;
-}
-
-
-  async deleteProductByCode(code) {
-    await this.loadProductsFromFile();
-    const index = this.products.findIndex((p) => p.code === code);
-    if (index === -1) {
-      throw new Error('Product not found');
+  async updateProduct(id, updatedFields) {
+    const product = this.products.find((p) => p.id === id);
+    if (!product) {
+      throw new Error("Product not found");
     }
-    const deletedProduct = this.products.splice(index, 1);
-    await this.saveProductsToFile(this.products);
+    Object.assign(product, updatedFields);
+    await this.saveProductsToFile(this.products); // Guarda los productos en el archivo
+  }
+
+  async updateProductByCode(code, updatedFields) {
+    const product = this.products.find((p) => p.code === code);
+    if (!product) {
+      throw new Error("Product not found");
+    }
+    Object.assign(product, updatedFields);
+    await this.saveProductsToFile(this.products); // Guarda los productos en el archivo
+  }
+
+  async deleteProductById(id) {
+    const productId = parseInt(id);
+    const index = this.products.findIndex((p) => p.id === productId);
+    if (index === -1) {
+      throw new Error("Product not found");
+    }
+    const deletedProduct = this.products.splice(index, 1)[0];
+    await this.saveProductsToFile(this.products); // Guarda los productos en el archivo
     return deletedProduct;
   }
-  
+
+  async deleteProductByCode(code) {
+    const index = this.products.findIndex((p) => p.code === code);
+    if (index === -1) {
+      throw new Error("Product not found");
+    }
+    const deletedProduct = this.products.splice(index, 1)[0];
+    await this.saveProductsToFile(this.products); // Guarda los productos en el archivo
+    return deletedProduct;
+  }
 
   generateProductId() {
-    let id = 1;
-    if (this.products.length > 0) {
-      const lastProduct = this.products[this.products.length - 1];
-      id = lastProduct.id + 1;
-    }
-    return id;
+    const lastProduct = this.products[this.products.length - 1];
+    const lastId = lastProduct ? lastProduct.id : 0;
+    return lastId + 1;
   }
 }
 
